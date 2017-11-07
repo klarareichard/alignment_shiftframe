bc70ab0dcd7090be14485cb04e63e8dca4b14844
Klara Reichard <klara@seoul.Informatik.Uni-Tuebingen.De>
2017-11-07 16:53:23 +0100

    updated scorematrix

--- ScoreMatrix.hpp
+++ ScoreMatrix.hpp
@@ -1,3 +1,57 @@
+#include <unordered_map>
+#include <pair>
+
+template<typename T>
 class ScoreMatrix{
-	
+	std::unordered_map< std::pair<T,T>, int > hash_map;
+	ScoreMatrix(){
+		auto pair1 = std::make_pair('A', 'A'); 
+		hash_map.insert({pair1, -1});
+
+		pair1 = std::make_pair('A', 'C'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('A', 'G'); 
+		hash_map.insert({pair1, 0.5});
+
+		pair1 = std::make_pair('A', 'T'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('C', 'A'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('C', 'C'); 
+		hash_map.insert({pair1, -1});
+
+		pair1 = std::make_pair('C', 'G'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('C', 'T'); 
+		hash_map.insert({pair1, 0.5});
+
+		pair1 = std::make_pair('G', 'A'); 
+		hash_map.insert({pair1, 0.5});
+
+		pair1 = std::make_pair('G', 'C'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('G', 'G'); 
+		hash_map.insert({pair1, -1});
+
+		pair1 = std::make_pair('G', 'T'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('T', 'A'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('T', 'C'); 
+		hash_map.insert({pair1, 0.5});
+
+		pair1 = std::make_pair('T', 'G'); 
+		hash_map.insert({pair1, 1});
+
+		pair1 = std::make_pair('T', 'T'); 
+		hash_map.insert({pair1, -1});
+
+	}
 }
