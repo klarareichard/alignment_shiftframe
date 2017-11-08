e40ea31937384d315b9687a2ae7f0303530940a8
Klara Reichard <klara@seoul.Informatik.Uni-Tuebingen.De>
2017-11-08 18:02:03 +0100

    added main.cpp

--- Sequence.hpp
+++ Sequence.hpp
@@ -1,6 +1,10 @@
+#include <string>
 template <typename T> class Sequence{
 
 		std::string<T> m_seq;
+
+		Sequence(std::string s):m_seq(seq){};
+		
 		void shift(int i){
 			m_seq = m_seq.substr(i, m_seq.length());
 		}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "alignment.hpp"
+#include "Matrix.hpp"
+#include "ScoreMatrix.hpp"
+
+int main(int argc, char * argv[])
+{
+   //code
+	Sequence seq1("ATCAAGAATTCTAATGTA");
+	Sequence seq2("MKIL");
+	Alignment al<Matrix, Sequence, ScoreMatrix>(seq1, seq2, 1, 1, 1, );
+}
